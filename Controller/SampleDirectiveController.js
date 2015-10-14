a63bb7cbaa450ac75842a2fb1a2aa410ed831ff1
six_sic6 <abedinyeasin@gmail.com>
2015-10-14 23:30:02 +0600

    Custom directives done

--- Controller/SampleDirectiveController.js
+++ Controller/SampleDirectiveController.js
@@ -2,8 +2,12 @@
 
 	var SampleDirectiveController = function ($scope, $location) {
 		$scope.name = 'Yeasin';
+		$scope.roll = 100;
+		$scope.country = 'Bangladesh';
+		$scope.email = 'test@gmail.com';
+		$scope.temp = 'Value from parent Scope';
 		$scope.getScopeValue = function(){
-			console.log($scope.name);
+			console.log('Name : ' + $scope.name + ' Roll : ' + $scope.roll + ' Country : ' + $scope.country);
 		};
 	};
 
--- Script/customDirective.js
+++ Script/customDirective.js
@@ -12,11 +12,15 @@ myApp.directive('myDirective1', function ($compile) {
 		// If we set the below attribute, custom directive will be replace by it's value.
 		// Here, we have an HTML markup as custom directive's value.
 		// Advantage : No need to compile.
-		template: "<label>Child Scope : </label> {{name}}" +
-				  "<br />" + 
-				  "<label>Child Scope Input : </label> <input type='text' ng-model='name' />" +
+		template: "<h4>Child Scope attributes</h4>" +
+				  "<hr />" +
+				  "<label>Name : </label> {{name}} <input type='text' ng-model='name' />" +
 				  "<br />" +
-				  "<button type='button' ng-click='action()'>Get Child Scope</button>",
+				 "<label>Roll : </label> {{roll}} <input type='text' ng-model='roll' />" +
+				 "<br />" +
+				 "<label>Country : </label> {{country}} <input type='text' ng-model='country' />" +
+				 "<br />" +
+				 "<button type='button' ng-click='action()'>Get Parent Scope</button>",
 		
 		// parameters of link function
 		// 1. element = element on which we are applying custom directive
@@ -43,13 +47,150 @@ myApp.directive('myDirective1', function ($compile) {
 		//scope: true
 		
 		scope:{
-			name: "@",
+			name: "@", // one way binding. Change in child scope will not affect parent
+			roll: "=", // two way binding. Change in child scope will affect parent
+			country: "=",
 			action: "&" // this will invoke controller's method. Idea is, if any change happens inside directive then we will have  chance to notify controller.
 		}
 		
 	};
 });
 
+myApp.directive('dateKeys', function () {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs, controller){
+			element.on('keydown', function(event){
+				if(isNumericKeyCode(event.keyCode) || isForwardSlashKeyCode(event.keyCode) || isNavigationKeyCode(event.keyCode)){
+					return true;					
+				}
+				return false;
+			});
+		}
+	}
+	
+	function isNumericKeyCode(keyCode){
+		return (keyCode >= 48 && keyCode <=  57)
+				|| (keyCode >= 96 && keyCode <=  105)
+	}
+	
+	function isForwardSlashKeyCode(keyCode){
+		return keyCode == 191;
+	}
+	
+	function isNavigationKeyCode(keyCode){
+		switch(keyCode){
+			case 8: // backspace
+			case 35:// end
+			case 36:// home
+			case 37:// left
+			case 38:// up
+			case 39:// right
+			case 40:// down
+			case 45:// ins
+			case 46:// del
+				return true;
+			default:
+				return false;
+		}
+	}
+});
+
+myApp.directive('funDirective', function () {
+	return {
+		restrict: 'E',
+		scope: true,
+		template: "<h4><input type='text' ng-model='temp' /> Status : {{directiveScope}}</h4>" + 
+				  "<br />" +
+				  "<button type='button' ng-click='directiveMethod()'>Directive's Controller Method</button>",	
+		controller: function($scope){ 
+			// directive's own controller
+			// but the $scope is prototypically inherited from controller
+			$scope.directiveScope = 'Value of Directive Scope';
+			$scope.directiveMethod = function(){
+				console.log("Directive's controller method");
+				console.log($scope.temp); // this will print parent cotroller's value
+				$scope.getScopeValue(); // this will call parent controller's method
+			}
+		}
+		/*
+		link: function(scope, element, attrs, controller){
+			attrs.$observe('email', function(newValue,oldValue){
+				if(newValue != oldValue){
+					scope.valueTracker = 'Changed';
+				}
+			});
+		}*/
+	}
+});
+
+// mutliple directive controllers can communicates with each other. See below :
+myApp.directive('greeting', function () {
+	return {
+		restrict: 'E',
+		replace: 'true',
+		transclude: true,
+		// this transclude property is required.
+		// this enables nested directives to comminicate with this directive
+		template: "<button class='btn' ng-click='sayHello()'>Directive Controller Communication</button>",
+		controller: function($scope){
+			var greetings = ['hello'];
+			$scope.sayHello = function(){
+				alert(greetings.join());
+			}
+			this.addGretting = function(greeting){
+				greetings.push(greeting);
+			}
+		}	
+	};
+})
+	.directive('finnish',function(){
+		return{
+			restrict: 'A',
+			require: '^greeting', 
+			//transclude: true,
+			// why this '^' symbol before drective name??
+			// require attribute tells name of the directive to which it will communicate
+			// require attribute directive must also be within the same element.
+			// It will not work if this directive is a nested element like :
+			// So, <greeting> <div finnish> </div> </greeting> will not work
+			// Only : <greetin finnish /> will work
+			// So, in order to work also for nested element, we specified '^'
+			link: function(scope, element, attrs, controller){
+				controller.addGretting('hei'); // this will call the method of a controller that has been defined in required attribute
+			}
+		}
+	})
+	.directive('bengali',function(){
+		return{
+			restrict: 'A',
+			require: '^greeting', 
+			//transclude: true,
+			// why this '^' symbol before drective name??
+			// require attribute tells name of the directive to which it will communicate
+			// require attribute directive must also be within the same element.
+			// It will not work if this directive is a nested element like :
+			// So, <greeting> <div bengali> </div> </greeting> will not work
+			// Only : <greetin bengali /> will work
+			// So, in order to work also for nested element, we specified '^'
+			link: function(scope, element, attrs, controller){
+				controller.addGretting('কেমন আছেন');  // this will call the method of a controller that has been defined in required attribute
+			}
+		}
+	});
+
+// Use Jquery methods inside custom directives
+myApp.directive('callJquery', function () {
+	return{
+		restrict: 'A',
+		link: function(scope, element){
+			element.click(function(event){
+				alert('Jqeury Method');
+			});
+		}
+	}
+});	
+	
 myApp.directive('myDirective2', function () {
 	return {
 		restrict: 'E',
